f0fe6cc01e9173947bbe580071c6dea14471bda1
Mike Faust <mike@elfausto.net>
2019-09-09 20:25:35 -0400

    remove scripts

--- scripts/orgInitPackage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sfdx force:org:create -a mclgtestInstall1 -f ../config/project-scratch-def.json
-
-sfdx force:package:install -u mclgtestInstall1 -p 04t3i000001OTnH -w 5
-
-sfdx force:org:open -u mclgtestInstall1
-
