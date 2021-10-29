3098584624aa2568cd7a9b3242b8a2ba0cf9a600
M Hamza Siddiqui <mhamzasite@gmail.com>
2021-10-29 10:01:24 -0400

    No need of Wrapper Component

--- force-app/main/default/aura/mortgageCalcWrapper/mortgageCalcWrapperController.js
+++
@@ -1,9 +0,0 @@
-({
-    handleMortgageChange : function(component, event) {
-        /* when a mortgage field changes, update the aura component attributes so that when passed back to the flow */
-        let eventParams = event.getParams('detail');
-        component.set("v.mortgageRate", eventParams.rate);
-        component.set("v.mortgageYears", eventParams.years);
-        component.set("v.mortgagePrincipal", eventParams.principal);
-    }
-})
